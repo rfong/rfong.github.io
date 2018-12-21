c12ce7455c1c699c30e567b2a2007ce7c07e93b4
rfong <rachefo@gmail.com>
2018-12-20 23:05:57 -0800

    linked distractibility cartographers

--- compile.sh
+++ compile.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 # Run in venv
 when-changed templates/*.html -c python compile.py
+
+# If you get an error like "invalid destination port", try:
+# 	rm ~/Library/Preferences/com.apple.imservice.ids.FaceTime.plist
